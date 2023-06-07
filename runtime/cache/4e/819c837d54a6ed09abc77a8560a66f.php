19a7b886875a8284f6b8b9451c4eafe8b5e51e26
lk <lzw10168@163.com>
2023-06-07 11:11:09 +0800

    feat: 删除缓存

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:5:{s:8:"sms_send";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:10:"sms_notice";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:9:"sms_check";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:8:"app_init";a:2:{i:0;s:23:"\addons\crontab\Crontab";i:1;s:17:"\addons\epay\Epay";}s:11:"config_init";a:1:{i:0;s:25:"\addons\nkeditor\Nkeditor";}}
