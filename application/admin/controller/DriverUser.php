3c51fe52c8be98b0249b05ce6f577c96461a620b
lizhiwei <lizhiwei@movee.cn>
2023-06-07 23:37:19 +0800

    feat: 获取最近的20个司机

--- application/admin/controller/DriverUser.php
+++ application/admin/controller/DriverUser.php
@@ -12,6 +12,8 @@ use think\Db;
 use think\Exception;
 use think\exception\PDOException;
 use think\exception\ValidateException;
+use think\Log;
+use Illuminate\Support\Arr;
 
 /**
  * 驾驶证认证管理
@@ -38,6 +40,8 @@ class DriverUser extends Backend
         parent::import();
     }
 
+    
+    
     /**
      * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
@@ -46,45 +50,84 @@ class DriverUser extends Backend
 
     public function index()
     {
-        // 过滤请求参数中的 HTML 标签和空格，并把它们作为筛选条件
+        //设置过滤方法
         $this->request->filter(['strip_tags', 'trim']);
-
-        // 判断是否是 AJAX 请求
         if ($this->request->isAjax()) {
-            // 如果请求中包含名为 keyField 的参数，则说明该请求是从 Selectpage 页面发送的，因此需要跳转到 Selectpage 页面处理
+            //如果发送的来源是Selectpage，则转发到Selectpage
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
-            
-            // 解析请求参数，包括筛选条件、排序方式、分页和每页数量等
-            list( $sort, $order, $offset, $limit) = $this->buildparams();
-            // 构造额外的筛选条件，只查询 driver_verified 表中 status 字段为 1 的记录
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            // 单独取search参数
+            $search = $this->request->request('search');
+            $where = (array)$where;
             $where2 = [];
             $where2['driver_verified.status'] = '1';
-            // 打印$where
-            // 使用 with 方法关联 user 表，查询满足 where 条件和 where2 条件的数据并按照 sort 和 order 排序，分页查询 limit 条数据
             $list = $this->model
                 ->with(['user'])
-                ->where($where)
+                // 从user表中取出username和mobile 匹配$search
+                ->where('user.username|user.mobile', 'like', '%' . $search . '%')
                 ->where($where2)
                 ->order($sort, $order)
                 ->paginate($limit);
-            
-            // 对查询结果进行遍历，只返回关联表 user 中的 username 和 mobile 字段数据
+            // 在日志中打印
             foreach ($list as $row) {
-                $row->getRelation('user')->visible(['username', 'mobile']);
+                $row->getRelation('user')->visible(['username', 'mobile', 'longitude', 'latitude']);
             }
-            
-            // 组合查询结果并以 JSON 格式返回
             $result = ["total" => $list->total(), "rows" => $list->items()];
+
             return json($result);
         }
-
-        // 如果不是 AJAX 请求，则渲染视图并返回
         return $this->view->fetch();
-
     }
 
+
+    function distance($lat1, $lon1, $lat2, $lon2, $unit = 'km', $decimal = 2) {
+      $theta = $lon1 - $lon2;
+      $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+      $dist = acos($dist);
+      $dist = rad2deg($dist);
+      $miles = $dist * 60 * 1.1515;
+      $unit = strtolower($unit);
+      if ($unit == "km") {
+          return round($miles * 1.609344, $decimal);
+      } else if ($unit == "nautical miles") {
+          return round($miles * 0.8684, $decimal);
+      } else {
+          return round($miles, $decimal);
+      }
+  }
+    
+    // 获取最近的20个司机
+    public function getNearbyDriver()
+    {
+
+      $longitude = $this->request->request('longitude');
+      $latitude = $this->request->request('latitude');
+      $where = [];
+      $where['driver_verified.status'] = '1';
+      $list = $this->model
+          ->with(['user'])
+          ->where($where)
+          ->select();
+      $result = [];
+      foreach ($list as $row) {
+          $row->getRelation('user')->visible(['username', 'mobile', 'longitude', 'latitude']);
+          $distance = $this->distance($latitude, $longitude, $row->user->latitude, $row->user->longitude);
+          $row->distance = $distance;
+          $result[] = $row;
+      }
+      //  按照距离排序
+      // 自定义排序算法
+
+
+// 使用usort()函数进行排序
+      usort($result, function ($a, $b) {
+        return $a['distance'] - $b['distance'];
+      });
+      $result = array_slice($result, 0, 20);
+      return json($result);
+    }
 
     /**
      * 批量操作
@@ -108,4 +151,65 @@ class DriverUser extends Backend
         }
         return parent::multi($ids);
     }
+
+    
+
+    public function getList()
+    {
+        $params = $this->request->request('params');
+        parse_str($params, $paramsArr);
+        Log::write($paramsArr);
+        //设置过滤方法
+        $this->request->filter(['strip_tags', 'trim']);
+
+            // list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            // 
+            // 定义默认的$sort $order $limit
+            $sort = 'id';
+            $order = 'desc';
+            $limit = 10;
+            // 从post请求单独取search参数
+            
+
+            $where2['driver_verified.status'] = '1';
+            $list = $this->model
+                ->with(['user'])
+                // 从user表中取出username和mobile 匹配$search
+                // ->where('user.username|user.mobile', 'like', '%' . $search . '%')
+                ->where($where2)
+                ->order($sort, $order)
+                ->paginate($limit);
+            // 在日志中打印
+            foreach ($list as $row) {
+                $row->getRelation('user')->visible(['username', 'mobile', 'longitude', 'latitude']);
+            }
+            // 把user内的数据取出来, 放到list里面
+            foreach ($list as $key => $value) {
+                $list[$key]['username'] = $value['user']['username'];
+                $list[$key]['mobile'] = $value['user']['mobile'];
+                // 把id 换成user_id
+                $list[$key]['id'] = $list[$key]['user_id'];
+            }
+            $result = ["total" => $list->total(), "list" => $list->items()];
+
+            return json($result);
+    }
+
+    // 写一个方法更新user表中的latitude longitude
+    public function updateLocation()
+    {
+      $user_id = $this->request->request('user_id');
+      $latitude = $this->request->request('latitude');
+      $longitude = $this->request->request('longitude');
+      Log::write($longitude);
+      if ($user_id) {
+        $res = Db::name('user')->where('id', $user_id)->update(['latitude' => $latitude, 'longitude' => $longitude]);
+        if ($res) {
+          $this->success('更新成功');
+        } else {
+          $this->error('更新失败');
+        }
+      }
+    }
 }
+
